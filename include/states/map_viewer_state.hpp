62cc8310761aa926a7a41d44f221fc67f863e86a
Derwins Ochoa <111207083+derwins8a@users.noreply.github.com>
2024-10-09 15:19:12 +0000

    feat: pan map view

--- include/states/map_viewer_state.hpp
+++ include/states/map_viewer_state.hpp
@@ -4,6 +4,7 @@
 #include "config/global.hpp"
 #include "states/state.hpp"
 #include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Text.hpp>
 #include <SFML/Window/Event.hpp>
@@ -12,6 +13,34 @@
 
 #include "engine/tmx_core.hpp"
 
+// class ViewDragger
+// {
+//   public:
+//     /// set render target with view and initialize dragging to false
+//     ViewDragger(sf::RenderTarget &target) : target{target}, dragging{}
+//     {
+//         std::cout << "VIEWDRAGGER CREATED!";
+//     }
+
+//     void setTarget(sf::RenderTarget &target2)
+//     {
+//         this->target = std::move(target2);
+//     }
+//     /// handle dragging related events
+//     void handleEvent(const sf::Event event)
+//     {
+
+//     }
+
+//   private:
+//     /// the render target with the view we want to change
+//     sf::RenderTarget &target;
+//     /// the last known mouse position
+//     sf::Vector2i previous_mouse_position;
+//     /// whether we are dragging or not
+//     bool dragging;
+//     GameDataRef _data;
+// };
 class MapViewerState : public Engine::State
 {
   public:
@@ -25,4 +54,5 @@ class MapViewerState : public Engine::State
 
   private:
     GameDataRef _data;
+    // ViewDragger view_dragger;
 };
--- source/states/map_viewer_state.cpp
+++ source/states/map_viewer_state.cpp
@@ -9,14 +9,64 @@ MapViewerState::MapViewerState(GameDataRef data) : _data(data)
 
 void MapViewerState::init_state()
 {
+    sf::View view(sf::FloatRect({0.f, 0.f}, {1000.f, 600.f}));
+
+    // activate it
+    this->_data->window->setView(view);
+    // this->view_dragger.setTarget(*this->_data->window);
 }
+
+/// the last known mouse position
+sf::Vector2i previous_mouse_position;
+/// whether we are dragging or not
+bool dragging = false;
 
 void MapViewerState::update_inputs()
 {
+    sf::RenderTarget &target{*this->_data->window};
     // Event Polling
     while (const std::optional event = this->_data->window->pollEvent())
     {
         this->_data->gui.handleEvent(*event);
+
+        if (const auto *keyPress = event->getIf<sf::Event::MouseButtonPressed>())
+        {
+            // if mouse button is pressed start dragging
+            if (keyPress->button == sf::Mouse::Button::Right)
+            {
+                dragging = true;
+                return;
+            }
+        }
+        if (const auto *keyPress = event->getIf<sf::Event::MouseButtonReleased>())
+        {
+            // if mouse button is released stop draggin
+            if (keyPress->button == sf::Mouse::Button::Right)
+            {
+                dragging = false;
+                return;
+            }
+        }
+        // if dragging mouse
+        if (const auto *keyPress = event->getIf<sf::Event::MouseMoved>())
+        {
+            // get mouse position
+            const sf::Vector2i mouse_position{keyPress->position.x, keyPress->position.y};
+            // if dragging, move view
+            if (dragging)
+            {
+                // calculate how far mouse has moved in view
+                const auto delta =
+                    target.mapPixelToCoords(mouse_position) - target.mapPixelToCoords(previous_mouse_position);
+                // apply negatively to view
+                auto view = target.getView();
+                view.move(-delta);
+                target.setView(view);
+            }
+            // update previous mouse position
+            previous_mouse_position = mouse_position;
+            return;
+        }
 
         if (event->is<sf::Event::Closed>())
         {
@@ -84,17 +134,16 @@ void MapViewerState::draw_state(float dt __attribute__((unused)))
     map.load("assets/maps/demo.tmx");
 
     MapLayer layerZero(map, 0);
-    // MapLayer layerOne(map, 1);
-    // MapLayer layerTwo(map, 2);
+    MapLayer layerOne(map, 1);
+    MapLayer layerTwo(map, 2);
 
     sf::Clock globalClock;
     sf::Time duration = globalClock.restart();
     layerZero.update(duration);
     this->_data->window->clear(sf::Color::Black);
     this->_data->window->draw(layerZero);
-    // this->_data->window->draw(layerOne);
-    // this->_data->window->draw(layerTwo);
-    this->_data->window->display();
+    this->_data->window->draw(layerOne);
+    this->_data->window->draw(layerTwo);
     // Displays rendered objects
     this->_data->window->display();
 }
