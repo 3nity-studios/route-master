56db2fc1a5e9d60023ef690fabc08bd1c3706bc5
Sebastian Ramirez <sebastian.jose.ramirez.a@gmail.com>
2024-10-09 21:59:20 -0400

    feat: Add tests for class Store

--- tests/src/player_tests.cpp
+++ tests/src/player_tests.cpp
@@ -50,12 +50,67 @@ TEST_CASE("Player get_bus", "[player_get_bus]") {
     REQUIRE(player.get_bus(2).get_name() == "Bus3");
 }
 
-// TEST_CASE("Player get_employee", "[player_get_employee]") {
-//     Player player(3, "Dave", 300);
-//     Employee emp1("Emp1", "LastName1", 30, 8, 0);
-//     Employee emp2("Emp2", "LastName2", 25, 8, 0);
-//     player.add_employee(emp1);
-//     player.add_employee(emp2);
-//     REQUIRE(player.get_employee(0).get_name() == "Emp1");
-//     REQUIRE(player.get_employee(1).get_name() == "Emp2");
-// }
+TEST_CASE("Player get_employee", "[player_get_employee]") {
+    Player player(3, "Dave", 300);
+    Employee emp1("Emp1", "LastName1", 30, 8, 0);
+    Employee emp2("Emp2", "LastName2", 25, 8, 0);
+    player.add_employee(emp1);
+    player.add_employee(emp2);
+    REQUIRE(player.get_employee(0).get_name() == "Emp1");
+    REQUIRE(player.get_employee(1).get_name() == "Emp2");
+}
+
+TEST_CASE("Store constructor", "[store_constructor]") {
+    Store store;
+    REQUIRE(store.get_inventory().size() == 0);
+    REQUIRE(store.get_bus_list().size() == 0);
+    REQUIRE(store.get_employee_list().size() == 0);
+}
+
+TEST_CASE("Store add_bus", "[store_add_bus]") {
+    Store store;
+    Bus bus(0, "Bus1", 50, {}, 0);
+    store.add_bus(bus);
+    REQUIRE(store.get_bus_list().size() == 1);
+    REQUIRE(store.get_bus_list()[0].get_name() == "Bus1");
+}
+
+TEST_CASE("Store add_employee", "[store_add_employee]") {
+    Store store;
+    Employee emp("Emp1", "LastName1", 30, 8, 0);
+    store.add_employee(emp);
+    REQUIRE(store.get_employee_list().size() == 1);
+    REQUIRE(store.get_employee_list()[0].get_name() == "Emp1");
+}
+
+TEST_CASE("Store set_inventory", "[store_set_inventory]") {
+    Store store;
+    Designar::SLList<Item> inventory;
+    Item item(0, "Item1", 10, 5, ItemType::Bus, false);
+    inventory.append(item);
+    store.set_inventory(inventory);
+    REQUIRE(store.get_inventory().size() == 1);
+    REQUIRE(store.get_inventory()[0].get_name() == "Item1");
+}
+
+TEST_CASE("Store buy_item", "[store_buy_item]") {
+    Store store;
+    Player player(1, "Alice", 1000);
+    Item item(0, "BusItem", 10, 100, ItemType::Bus, false);
+    Bus bus(0, "Bus1", 50, {}, 0);
+    store.set_inventory({item});
+    store.add_bus(bus);
+    store.buy_item(player, 0, 1);
+    REQUIRE(player.get_balance() == 990);
+    REQUIRE(player.get_buses().size() == 1);
+    REQUIRE(player.get_buses()[0].get_name() == "Bus1");
+}
+
+TEST_CASE("Store buy_bus_maintenance", "[store_buy_bus_maintenance]") {
+    Store store;
+    Player player(1, "Alice", 10000);
+    Bus bus(0, "Bus1", 50, {}, 0);
+    store.add_bus(bus);
+    store.buy_bus_maintenance(bus, player, true, true, true);
+    REQUIRE(player.get_balance() < 10000);
+}
