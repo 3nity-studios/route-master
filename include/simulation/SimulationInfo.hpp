09873ac467b7657f77a4164b11ce748ff9e79b43
Jesús <jesusalfonso1225@gmail.com>
2024-11-09 15:20:37 -0400

    refactor: SimulationInfo and Route now have their own files

---
+++ include/simulation/SimulationInfo.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "simulation/Bus.hpp"
+#include "simulation/Employee.hpp"
+#include "simulation/Street.hpp"
+#include <Designar/graph.hpp>
+#include <nlohmann/json.hpp>
+
+using StreetArcList = Designar::SLList<Designar::GraphArc<Designar::GraphNode<std::shared_ptr<VisualElement>, Street, Designar::EmptyClass>, std::shared_ptr<VisualElement>, Street, Designar::EmptyClass> *>;
+
+struct SimulationInfo
+{
+    Bus *bus;
+    Employee *employee;
+    std::vector<std::shared_ptr<VisualElement>> elements_path;
+    std::pair<int, int> time_state;
+    int path_index;
+    int previous_time;
+    bool next_is_street;
+    bool route_completed;
+    bool isVisible; 
+    sf::Clock projection_clock;
+    sf::Sprite projection_bus;
+    sf::Vector2f projection_bus_speed;
+    sf::Texture projection_bus_texture;
+
+    std::vector<std::string> track_names;
+    std::vector<int> track_times;
+    std::vector<std::string> passenger_stop_names;
+    std::vector<std::pair<int, int>> passengers_per_stop;
+
+    SimulationInfo(Bus *_bus, Employee *_employee, StreetArcList _path); 
+        
+    void set_path(StreetArcList path); 
+    nlohmann::json to_json(); 
+    sf::Vector2f calc_vector_tip(); 
+};
---
+++ source/simulation/SimulationInfo.cpp
@@ -0,0 +1,88 @@
+#include "simulation/SimulationInfo.hpp"
+
+SimulationInfo::SimulationInfo(Bus *_bus, Employee *_employee, StreetArcList _path) : bus(_bus), employee(_employee), time_state(std::make_pair<int, int>(-1, 0)), path_index(0),
+          next_is_street(false), route_completed(false), projection_bus_texture(sf::Texture()),
+          projection_bus(projection_bus_texture)
+{
+    set_path(_path);
+}
+
+void SimulationInfo::set_path(StreetArcList path)
+{
+    elements_path.clear();
+
+    if (!path.is_empty())
+    {
+        for (auto track : path)
+        {
+            elements_path.push_back(track->get_src_node()->get_info());
+        }
+
+        elements_path.push_back(path.get_last()->get_tgt_node()->get_info());
+    }
+}
+
+nlohmann::json SimulationInfo::to_json()
+{
+    projection_clock.stop(); 
+
+    nlohmann::json j; 
+
+    j["bus"] = bus->to_json();
+    j["employee"] = employee->to_json(); 
+       
+    nlohmann::json elements_path_json = nlohmann::json::array();
+    for (auto visual_element : elements_path)
+    {
+        elements_path_json.push_back(visual_element->to_json());
+    }
+
+    j["elements_path"] = elements_path_json;
+    j["time_state_first"] = time_state.first;
+    j["time_state_second"] = time_state.second;
+    j["path_index"] = path_index; 
+    j["next_is_street"] = next_is_street;
+    j["route_completed"] = route_completed; 
+
+    nlohmann::json times_vector = nlohmann::json::array();
+    for (auto time : track_times)
+    {
+        times_vector.push_back(time);
+    }
+
+    j["times"] = times_vector;
+
+    nlohmann::json passengers_per_stop_first = nlohmann::json::array();
+    nlohmann::json passengers_per_stop_second = nlohmann::json::array(); 
+
+    for (auto passengers : passengers_per_stop)
+    {
+        passengers_per_stop_first.push_back(passengers.first);
+        passengers_per_stop_second.push_back(passengers.second);
+    }
+
+    j["passengers_stop_first"] = passengers_per_stop_first;
+    j["passengers_stop_second"] = passengers_per_stop_second;
+
+    return j; 
+}
+
+sf::Vector2f SimulationInfo::calc_vector_tip() 
+{
+    float bus_speed_module = sqrt((projection_bus_speed.x * projection_bus_speed.x) + (projection_bus_speed.y * projection_bus_speed.y));
+
+    sf::Vector2f norm_direction_vector;
+
+    if (bus_speed_module == 0)
+    {
+        norm_direction_vector = sf::Vector2f(0.f, 0.f);
+    }
+    else
+    {
+        norm_direction_vector = projection_bus_speed / bus_speed_module;
+    }
+
+    sf::Vector2f position_vector_tip = projection_bus.getPosition() + (norm_direction_vector * 60.f);
+
+    return position_vector_tip;
+}
