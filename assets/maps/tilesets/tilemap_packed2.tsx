07a744c5f9cafca21486b8b51bb38865f07a5e6f
Derwins Ochoa <111207083+derwins8a@users.noreply.github.com>
2024-11-10 17:35:49 +0000

    feat: new map n load once

---
+++ assets/maps/tilesets/tilemap_packed2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tilemap_packed" tilewidth="32" tileheight="32" tilecount="702" columns="27">
+ <image source="../../img/tilesets/tilemap_packed.png" width="864" height="832"/>
+</tileset>
